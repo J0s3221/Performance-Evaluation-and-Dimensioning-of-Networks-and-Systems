b8d0e9f18810f70693e2a0500a35e26387a3ac9a
J0s3221 <athydeforking@gmail.com>
2026-09-15 20:37:22 +0100

    feat: added the script for the exercise 7

---
+++ module 1/scripts/E7_MM2Queue.R
@@ -0,0 +1,41 @@
+ArrivalRate=1
+ServiceRate=2
+Time=0
+NumQueueCompleted=0
+ServerStatus=c(0,0)          # 0 = idle, 1 = busy, one entry per server
+NumInQueue=0
+AcumDelay=0
+QueueArrivalTime=c()
+EventList=c(rexp(1,ArrivalRate), Inf, Inf)  # [arrival, dep@server1, dep@server2]
+
+while (NumQueueCompleted<1000) {
+  NextEventType=which.min(EventList)
+  Time=EventList[NextEventType]
+
+  if (NextEventType==1) {                    # ARRIVAL
+    EventList[1]=Time+rexp(1,ArrivalRate)
+    if (all(ServerStatus==1)) {               # both servers busy -> join queue
+      QueueArrivalTime=c(QueueArrivalTime,Time)
+      NumInQueue=NumInQueue+1
+    } else {                                  # a server is free -> start service
+      freeServer=which(ServerStatus==0)[1]
+      ServerStatus[freeServer]=1
+      NumQueueCompleted=NumQueueCompleted+1
+      EventList[freeServer+1]=Time+rexp(1,ServiceRate)
+    }
+  } else {                                    # DEPARTURE from server (NextEventType-1)
+    server=NextEventType-1
+    if (NumInQueue==0) {
+      ServerStatus[server]=0
+      EventList[NextEventType]=Inf
+    } else {
+      AcumDelay=AcumDelay+Time-QueueArrivalTime[1]
+      QueueArrivalTime=QueueArrivalTime[-1]
+      NumInQueue=NumInQueue-1
+      NumQueueCompleted=NumQueueCompleted+1
+      EventList[NextEventType]=Time+rexp(1,ServiceRate)
+    }
+  }
+}
+
+AcumDelay/NumQueueCompleted
